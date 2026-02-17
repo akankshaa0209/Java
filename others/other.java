2b3d765b3a89ea11539859129cdf15c6c521cc78
akankshaa0209 <akankshatripathi0609@gmail.com>
2026-02-17 22:57:11 +0530

    Initial commit

---
+++ others/other.java
@@ -0,0 +1,18 @@
+//package com.hp.others;
+
+//import com.hp.demu.helper;
+//
+//public class other {
+//    public static void main(String[] args) {
+//        helper h = new helper();  // this will work, if we do class and func public in helper.java
+//        h.greet();
+//    }
+//}
+
+//public class other {
+//    public static void main(String[] args) {
+//        helper h = new helper();  // ❌ ERROR: Helper is not public in com.hp.demo
+//        h.greet();
+//    }
+//}
+
